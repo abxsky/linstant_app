a8a9de939b4e6b095e12458160368969bfaacdb0
abxsky <auahabix@gmail.com>
2024-03-12 17:49:41 +0200

    register form validation

--- lib/forms/register_form.dart
+++ lib/forms/register_form.dart
@@ -1,8 +1,20 @@
 import 'package:flutter/material.dart';
 import 'package:linstant_app/forms/form_fields/form_text_field.dart';
+import 'package:email_validator/email_validator.dart';
 
-class RegisterForm extends StatelessWidget {
+class RegisterForm extends StatefulWidget {
   const RegisterForm({super.key});
+
+  @override
+  State<RegisterForm> createState() => _RegisterFormState();
+}
+
+class _RegisterFormState extends State<RegisterForm> {
+  final _formKey = GlobalKey<FormState>();
+
+  void _saveUser() {
+    _formKey.currentState!.validate();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -19,8 +31,14 @@ class RegisterForm extends StatelessWidget {
             ),
             FormTextField(
               title: "Nom d'utilisateur",
-              validator: ((value) {
-                return "Nom d'utilisateur";
+              validator: ((username) {
+                if (username == null ||
+                    username.isEmpty ||
+                    username.trim().length <= 1 ||
+                    username.trim().length > 25) {
+                  return "le nom d'utilisateur n'est pas valide ";
+                }
+                return null;
               }),
             ),
             SizedBox(
@@ -28,8 +46,14 @@ class RegisterForm extends StatelessWidget {
             ),
             FormTextField(
               title: "E-mail",
-              validator: ((value) {
-                return "email";
+              validator: ((email) {
+                if (email == null ||
+                    EmailValidator.validate(email) ||
+                    email.trim().length < 3 ||
+                    email.trim().length > 50) {
+                  return 'Veuillez rensegnier un e-mail vailde.';
+                }
+                return null;
               }),
             ),
             SizedBox(
@@ -37,14 +61,22 @@ class RegisterForm extends StatelessWidget {
             ),
             FormTextField(
                 title: 'Mot de passe',
-                validator: (value) {
-                  return 'Mot de passe';
+                validator: (password) {
+                  RegExp regex = RegExp(
+                      r'^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[!@#\$&*~]).{8,}$');
+                  if (password == null ||
+                      password.isEmpty ||
+                      password.length < 8 ||
+                      !regex.hasMatch(password)) {
+                    return 'Le mot de passe doit contenir au moins une lettre minuslcule, une majuscule, un nombre, un charactére speciale et doit faire au moins 8 charactéres';
+                  }
+                  return null;
                 }),
             SizedBox(
               height: MediaQuery.of(context).devicePixelRatio * 30,
             ),
             ElevatedButton(
-              onPressed: () {},
+              onPressed: _saveUser,
               style: const ButtonStyle(
                   backgroundColor: MaterialStatePropertyAll(
                       Color.fromARGB(255, 245, 245, 76))),
--- lib/screens/register_screen.dart
+++ lib/screens/register_screen.dart
@@ -2,16 +2,9 @@ import 'package:flutter/material.dart';
 import 'package:linstant_app/forms/register_form.dart';
 import 'package:linstant_app/widgets/main_gradient_container.dart';
 
-class RegisterScreen extends StatefulWidget {
+class RegisterScreen extends StatelessWidget {
   const RegisterScreen({super.key});
 
-  @override
-  State<RegisterScreen> createState() {
-    return _RegisterScreenState();
-  }
-}
-
-class _RegisterScreenState extends State<RegisterScreen> {
   @override
   Widget build(BuildContext context) {
     return GestureDetector(
